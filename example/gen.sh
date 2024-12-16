7a4cdcf7d9c0808ed688940b29d8de7726aebc29
sang <sang.chen@outlook.com>
2024-12-16 17:39:47 +0800

    chore: update example

--- example/gen.sh
+++ example/gen.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-pushd ..
-    bash ./build/build.sh
-popd
+git_root=$(git rev-parse --show-toplevel)
+
+if [[ ! -f "${git_root}/dist/bin/protoc-gen-example" ]]; then
+    pushd "${git_root}" || return
+        bash ./build/build.sh
+    popd || return
+fi
 
 protoc \
     -I . \
-    -I ../proto \
-    --plugin=protoc-gen-example=../dist/bin/protoc-gen-example \
-    --example_out=. \
+    -I "${git_root}/proto" \
+    --plugin=protoc-gen-example="${git_root}/dist/bin/protoc-gen-example" \
+    --example_out=./pkg/api/v1 \
     hello.proto
